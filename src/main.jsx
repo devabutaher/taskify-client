b7287545a2921c46682c25fe5409f9bb50b96967
Abu Taher <writerabutaher@gmail.com>
2024-10-10 13:02:09 +0600

    setup redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
+import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <NextUIProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </NextUIProvider>
   </StrictMode>
 );
